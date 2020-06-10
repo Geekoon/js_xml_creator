892a9f83235cb6906130b958dd8c2e524c18c9bf
Anthony <orlovskikh@gmail.com>
2020-06-10 11:44:44 +0300

    Вывод списка категорий

--- yml_01/main.go
+++ yml_01/main.go
@@ -13,10 +13,16 @@ import (
 var database *sql.DB
 
 type Product struct {
-	Id   int
-	Name string
-	Url  string
+	id   int
+	name string
+	url  string
 }
+
+type GroupProduct struct {
+	id   int
+	name string
+}
+
 type Offer struct {
 	XMLName xml.Name `xml:"offer"`
 	Id      int      `xml:"id"`
@@ -24,19 +30,37 @@ type Offer struct {
 	Url     string   `xml:"url"`
 }
 
-type OfferArray struct {
-	Offers []Offer
+type Category struct {
+	XMLName xml.Name `xml:"category"`
+	Id      int      `xml:"id,attr"`
+	Name    string   `xml:",chardata"`
 }
 
-type AllOffers struct {
+type OfferArray struct {
 	XMLName xml.Name `xml:"offers"`
-	Offers  OfferArray
+	Offers  []Offer
+}
+
+type CategoryArray struct {
+	XMLName    xml.Name `xml:"categories"`
+	Categories []Category
+}
+
+type TagShop struct {
+	XMLName    xml.Name `xml:"shop"`
+	Categories CategoryArray
+	AllOffers  OfferArray
+}
+
+type YmlCatalog struct {
+	XMLName xml.Name `xml:"yml_catalog"`
+	Shop    TagShop
 }
 
 func getProduct() []Product {
 	rows, err := database.Query("select id, name, url from js78base.tbl_core AS c WHERE c.model='ProductItem'")
 	if err != nil {
-		log.Println(err)
+		log.Println("MySQL Error:", err)
 	}
 	defer rows.Close()
 
@@ -44,20 +68,45 @@ func getProduct() []Product {
 
 	for rows.Next() {
 		p := Product{}
-		err := rows.Scan(&p.Id, &p.Name, &p.Url)
+		err := rows.Scan(&p.id, &p.name, &p.url)
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 		products = append(products, p)
 	}
-
 	return products
+}
+
+func getCategory() []GroupProduct {
+	rows, err := database.Query("select id, name from js78base.tbl_product_item_kind AS pk")
+	if err != nil {
+		log.Println("MySQL Error:", err)
+	}
+	defer rows.Close()
+
+	var gp []GroupProduct
+
+	for rows.Next() {
+		p := GroupProduct{}
+		err := rows.Scan(&p.id, &p.name)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		gp = append(gp, p)
+	}
+	return gp
 }
 
 func (s *OfferArray) AddOffer(sId int, sName string, sUrl string) {
 	staffRecord := Offer{Id: sId, Name: sName, Url: sUrl}
 	s.Offers = append(s.Offers, staffRecord)
+}
+
+func (s *CategoryArray) AddCategory(sId int, sName string) {
+	staffRecord := Category{Id: sId, Name: sName}
+	s.Categories = append(s.Categories, staffRecord)
 }
 
 func main() {
@@ -70,36 +119,35 @@ func main() {
 	database = db
 	defer db.Close()
 
-	//fmt.Println(getProduct()[100])
+	pDB := getProduct()
+	catDB := getCategory()
 
-	filename := "ppp.txt"
-	file, err := os.Create(filename)
-	if err != nil {
-		fmt.Println("Unable to save file:", err)
-		os.Exit(1)
+	/*	filename := "ppp.txt"
+		file, err := os.Create(filename)
+		if err != nil {
+			fmt.Println("Unable to save file:", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		for i := 0; i < len(catDB); i++ {
+			fmt.Fprintln(file, catDB[i].id, catDB[i].name)
+		}
+	*/
+	var v = YmlCatalog{}
+	for i := 0; i < len(catDB); i++ {
+		v.Shop.Categories.AddCategory(catDB[i].id, catDB[i].name)
 	}
-	defer file.Close()
-
-	p := getProduct()
-	//fmt.Printf("%T", p)
-	for i := 0; i < len(p); i++ {
-		fmt.Fprintln(file, p[i].Id, p[i].Name)
-		//fmt.Println(p)
-	}
-
-	var v = AllOffers{}
-	for i := 0; i < len(p); i++ {
-		v.Offers.AddOffer(p[i].Id, p[i].Name, p[i].Url)
-	}
-
-	xmlString, err := xml.MarshalIndent(v, "", "    ")
-	if err != nil {
-		fmt.Println("Error in Marshal: ", err)
+	for i := 0; i < len(pDB); i++ {
+		v.Shop.AllOffers.AddOffer(pDB[i].id, pDB[i].name, pDB[i].url)
 	}
 
-	fmt.Printf("%s \n", string(xmlString))
+	/*	xmlString, err := xml.MarshalIndent(v, "", "    ")
+		if err != nil {
+			fmt.Println("Error in MarshalIndent: ", err)
+		}
+		fmt.Printf("%s \n", string(xmlString))
+	*/
 
-	// everything ok now, write to file.
 	xmlFileName := "offers.xml"
 	xmlFile, err := os.Create(xmlFileName)
 	if err != nil {
@@ -107,11 +155,12 @@ func main() {
 		os.Exit(1)
 	}
 	defer xmlFile.Close()
-
 	xmlWriter := io.Writer(xmlFile)
 
+	xmlWriter.Write([]byte(xml.Header))
+
 	enc := xml.NewEncoder(xmlWriter)
-	enc.Indent("  ", "    ")
+	enc.Indent("", "    ")
 	if err := enc.Encode(v); err != nil {
 		fmt.Printf("error: %v\n", err)
 	}
