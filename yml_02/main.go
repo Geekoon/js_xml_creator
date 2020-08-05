c00ce6ac06e701b9424a96dd1a93917095040780
Anthony <orlovskikh@gmail.com>
2020-08-05 18:34:55 +0300

    add log file and fix NULL

--- yml_02/main.go
+++ yml_02/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"encoding/xml"
-	"fmt"
 	"io"
 	"log"
 	"os"
@@ -11,42 +10,67 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+var (
+	WarningLogger *log.Logger
+	InfoLogger    *log.Logger
+	ErrorLogger   *log.Logger
+)
+
 var database *sql.DB
 
+func init() {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+	WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+}
+
+// Product is an offer comes from base with some common good's elements
 type Product struct {
 	id          int
-	group_id    int
-	parent_id   int
+	groupID     int
+	parentID    int
 	nameProduct string
 	nameOffer   string
 	url         string
 	description string
-	barcode     int
+	barcode     string
 	uuid        string
 	amount      float32
 }
 
+// GroupProduct is a folder (from 1C) or group of goods collected together by one feature or BRAND
 type GroupProduct struct {
-	id        int
-	parent_id int
-	name      string
+	id       int
+	parentID int
+	name     string
 }
 
 type Offer struct {
-	XMLName    xml.Name `xml:"offer"`
-	Id         int      `xml:"id,attr"`
-	Group_id   int      `xml:"group_id,attr"`
-	Uuid       string   `xml:"uuid"`
-	Url        string   `xml:"url"`
-	Name       string   `xml:"name"`
-	CountItems int      `xml:"countItems"`
+	XMLName     xml.Name     `xml:"offer"`
+	ID          int          `xml:"id,attr"`
+	GroupID     int          `xml:"group_id,attr"`
+	UUID        string       `xml:"uuid"`
+	URL         string       `xml:"url"`
+	Name        string       `xml:"name"`
+	CountItems  int          `xml:"countItems"`
+	Description *Description `xml:"description"`
+}
+
+type Description struct {
+	XMLName xml.Name `xml:"description"`
+	Text    string   `xml:",cdata"`
 }
 
 type Category struct {
-	XMLName   xml.Name `xml:"category"`
-	Id        int      `xml:"id,attr"`
-	Parent_id int      `xml:"parent_id,attr"`
-	Name      string   `xml:",chardata"`
+	XMLName  xml.Name `xml:"category"`
+	ID       int      `xml:"id,attr"`
+	ParentID int      `xml:"parent_id,attr"`
+	Name     string   `xml:",chardata"`
 }
 
 type OfferArray struct {
@@ -72,9 +96,9 @@ type YmlCatalog struct {
 
 func getProduct() []Product {
 
-	rows, err := database.Query("SELECT o.id, c.id, c.parent_id, c.name, o.name, c.url, c.content, o.barcode, o.id_1c_offer, ob.value FROM tbl_offers AS o LEFT OUTER JOIN tbl_core AS c ON o.id_product_item = c.id LEFT OUTER JOIN tbl_offer_balance AS ob ON o.id = ob.id_offer WHERE o.act=1 AND o.id_1c_offer != 0 AND ob.id_storage=2 AND ob.value != 0 LIMIT 3000")
+	rows, err := database.Query("SELECT o.id, c.id, c.parent_id, c.name, o.name, c.url, IFNULL(c.content, '') AS content, IFNULL(o.barcode, '') AS barcode, o.id_1c_offer, ob.value FROM tbl_offers AS o LEFT OUTER JOIN tbl_core AS c ON o.id_product_item = c.id LEFT OUTER JOIN tbl_offer_balance AS ob ON o.id = ob.id_offer WHERE o.act=1 AND o.id_1c_offer != 0 AND ob.id_storage=2 AND ob.value != 0 LIMIT 3000")
 	if err != nil {
-		log.Println("MySQL Error:", err)
+		ErrorLogger.Println("MySQL Error:", err)
 	}
 	defer rows.Close()
 
@@ -82,9 +106,9 @@ func getProduct() []Product {
 
 	for rows.Next() {
 		p := Product{}
-		err := rows.Scan(&p.id, &p.group_id, &p.parent_id, &p.nameOffer, &p.nameProduct, &p.url, &p.description, &p.barcode, &p.uuid, &p.amount)
+		err := rows.Scan(&p.id, &p.groupID, &p.parentID, &p.nameOffer, &p.nameProduct, &p.url, &p.description, &p.barcode, &p.uuid, &p.amount)
 		if err != nil {
-			fmt.Println(err)
+			ErrorLogger.Println(err)
 			continue
 		}
 		products = append(products, p)
@@ -96,7 +120,7 @@ func getGroups() []GroupProduct {
 	//rows, err := database.Query("select id, name from js78base.tbl_product_item_kind AS pk")
 	rows, err := database.Query("SELECT c.id, c.parent_id, c.name FROM tbl_core AS c WHERE model='ProductGroup' AND act=1")
 	if err != nil {
-		log.Println("MySQL Error:", err)
+		ErrorLogger.Println("MySQL Error:", err)
 	}
 	defer rows.Close()
 
@@ -104,9 +128,9 @@ func getGroups() []GroupProduct {
 
 	for rows.Next() {
 		p := GroupProduct{}
-		err := rows.Scan(&p.id, &p.parent_id, &p.name)
+		err := rows.Scan(&p.id, &p.parentID, &p.name)
 		if err != nil {
-			fmt.Println(err)
+			ErrorLogger.Println(err)
 			continue
 		}
 		gp = append(gp, p)
@@ -114,22 +138,21 @@ func getGroups() []GroupProduct {
 	return gp
 }
 
-func (s *CategoryArray) AddCategory(sId int, sParent_id int, sName string) {
-	staffRecord := Category{Id: sId, Parent_id: sParent_id, Name: sName}
+func (s *CategoryArray) AddCategory(sID int, sParentID int, sName string) {
+	staffRecord := Category{ID: sID, ParentID: sParentID, Name: sName}
 	s.Categories = append(s.Categories, staffRecord)
 }
 
-func (s *OfferArray) AddOffer(sId int, sGroup_id int, sUuid string, sAmount int, sName string, sUrl string) {
-	staffRecord := Offer{Id: sId, Group_id: sGroup_id, Uuid: sUuid, CountItems: sAmount, Name: sName, Url: sUrl}
+func (s *OfferArray) AddOffer(sID int, sGroupID int, sUUID string, sAmount int, sName string, sURL string, sDescription string) {
+	staffRecord := Offer{ID: sID, GroupID: sGroupID, UUID: sUUID, CountItems: sAmount, Name: sName, URL: sURL, Description: &Description{Text: sDescription}}
 	s.Offers = append(s.Offers, staffRecord)
 }
 
 func main() {
 
-	db, err := sql.Open("mysql", "admitex:8E5s3T7y2Y0w2W5y@/js2base")
-	//db, err := sql.Open("mysql", "antor:Yehat13@/js78base")
+	db, err := sql.Open("mysql", "antor:Yehat13@/js78base")
 	if err != nil {
-		log.Println(err)
+		ErrorLogger.Println(err)
 	}
 
 	database = db
@@ -140,16 +163,16 @@ func main() {
 
 	var v = YmlCatalog{}
 	for i := 0; i < len(catDB); i++ {
-		v.Shop.Categories.AddCategory(catDB[i].id, catDB[i].parent_id, catDB[i].name)
+		v.Shop.Categories.AddCategory(catDB[i].id, catDB[i].parentID, catDB[i].name)
 	}
 	for i := 0; i < len(pDB); i++ {
-		v.Shop.AllOffers.AddOffer(pDB[i].id, pDB[i].group_id, pDB[i].uuid, int(pDB[i].amount), pDB[i].nameOffer, pDB[i].url)
+		v.Shop.AllOffers.AddOffer(pDB[i].id, pDB[i].groupID, pDB[i].uuid, int(pDB[i].amount), pDB[i].nameOffer, pDB[i].url, pDB[i].description)
 	}
 
 	xmlFileName := "offers.xml"
 	xmlFile, err := os.Create(xmlFileName)
 	if err != nil {
-		fmt.Println("Unable to save XML file:", err)
+		ErrorLogger.Println("Unable to save XML file:", err)
 		os.Exit(1)
 	}
 	defer xmlFile.Close()
@@ -160,7 +183,7 @@ func main() {
 	enc := xml.NewEncoder(xmlWriter)
 	enc.Indent("", "    ")
 	if err := enc.Encode(v); err != nil {
-		fmt.Printf("error: %v\n", err)
+		ErrorLogger.Printf("error: %v\n", err)
 	}
 
 }
